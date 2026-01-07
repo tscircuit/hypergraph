5de766bc4660b1d5ea4430371f56b9edd2831c4e
seveibar <seveibar@gmail.com>
2026-01-07 10:12:49 -0800

    one test per file

--- tests/jumper-graph-generation/jumper-graph-generation06.test.ts
+++ tests/jumper-graph-generation/jumper-graph-generation06.test.ts
@@ -3,16 +3,6 @@ import { getSvgFromGraphicsObject } from "graphics-debug"
 import { generateSingleJumperX4Regions } from "lib/JumperGraphSolver/jumper-graph-generator/generateSingleJumperX4Regions"
 import { visualizeJumperGraph } from "lib/JumperGraphSolver/visualizeJumperGraph"
 
-test("jumper-graph-generation06 - 1206x4 resistor chip array", () => {
-  const singleJumperX4Topology = generateSingleJumperX4Regions({
-    center: { x: 0, y: 0 },
-    idPrefix: "jumperX4",
-  })
-  expect(
-    getSvgFromGraphicsObject(visualizeJumperGraph(singleJumperX4Topology)),
-  ).toMatchSvgSnapshot(import.meta.path)
-})
-
 test("jumper-graph-generation06 - 1206x4 with regionsBetweenPads", () => {
   const singleJumperX4Topology = generateSingleJumperX4Regions({
     center: { x: 0, y: 0 },
