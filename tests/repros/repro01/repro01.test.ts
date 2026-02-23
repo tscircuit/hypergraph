6359f2e64ac2f054415cdabbe266f50a21b88fa5
seveibar <seveibar@gmail.com>
2026-02-22 20:48:40 -0800

    add reproduction for failing jumper graph

--- tests/repros/repro01/repro01.test.ts
+++ tests/repros/repro01/repro01.test.ts
@@ -3,15 +3,8 @@ import { getSvgFromGraphicsObject } from "graphics-debug"
 import { JumperGraphSolver } from "lib/JumperGraphSolver/JumperGraphSolver"
 import repro01 from "./repro01.json"
 
-test("repro01 svg snapshot", () => {
-  const inputGraph = {
-    ...repro01.inputGraph,
-    ports: repro01.inputGraph.ports.map(({ regionIds, ...port }) => ({
-      ...port,
-      region1Id: regionIds[0],
-      region2Id: regionIds[1],
-    })),
-  }
+test.skip("repro01 svg snapshot", () => {
+  const inputGraph = repro01.inputGraph
 
   const solver = new JumperGraphSolver({
     inputGraph: inputGraph as any,
